08a6362689e2e26b411ab356a96d23276b0b8718
thrnkk <thomas.ricardo.reinke@gmail.com>
2022-05-14 05:45:57 -0300

    separate code and code correction

---
+++ key_expansion.py
@@ -0,0 +1,89 @@
+from variables import s_box, l_table, e_table
+from utils import string_to_hex_array, array_to_matrix, array_xor_array
+
+
+class KeyExpansion(object):
+
+    def __init__(self) -> None:
+        self.key = []
+        self.round_constant = (
+            '0x01', '0x02', '0x04', '0x08', '0x10', '0x20', '0x40', '0x80', '0x1b', '0x36'
+        )
+
+    def get_last_round_key(self) -> list:
+
+        round_key = [[], [], [], []]
+
+        initial_pos = len(self.key[0]) - 4
+        final_pos = len(self.key[0])
+
+        round_key[0] = self.key[0][initial_pos : final_pos]
+        round_key[1] = self.key[1][initial_pos : final_pos]
+        round_key[2] = self.key[2][initial_pos : final_pos]
+        round_key[3] = self.key[3][initial_pos : final_pos]
+
+        return round_key
+
+    def get_current_round_key(self, position) -> list:
+
+        round_key = [[], [], [], []]
+
+        initial_pos = position * 4
+        final_pos = initial_pos + 4
+
+        round_key[0] = self.key[0][initial_pos : final_pos]
+        round_key[1] = self.key[1][initial_pos : final_pos]
+        round_key[2] = self.key[2][initial_pos : final_pos]
+        round_key[3] = self.key[3][initial_pos : final_pos]
+
+        return round_key
+
+    def get_word(self, position) -> list:
+
+        round_key = self.get_last_round_key()
+
+        return [i[position] for i in round_key]
+
+    def rotate_bytes(self, a) -> list:
+
+        return a[1:] + a[:1]
+
+    def replace_word(self, word) -> list:
+
+        return [hex(s_box[int(byte, 16)]) for byte in word]
+
+    def get_round_constant(self, position) -> list:
+
+        return [self.round_constant[position], '0x00', '0x00', '0x00']
+
+    def key_add_word(self, word) -> None:
+
+        self.key[0].append(word[0])
+        self.key[1].append(word[1])
+        self.key[2].append(word[2])
+        self.key[3].append(word[3])
+
+    def expand_key(self, key) -> None:
+
+        self.key = array_to_matrix(string_to_hex_array(key))
+
+        for i in range(10):
+
+            last_word = self.get_word(3)
+            rotated = self.rotate_bytes(last_word)
+            replaced = self.replace_word(rotated)
+            round_constant = self.get_round_constant(i)
+
+            xored = array_xor_array(replaced, round_constant)
+
+            word1 = array_xor_array(self.get_word(0), xored)
+            word2 = array_xor_array(self.get_word(1), word1)
+            word3 = array_xor_array(self.get_word(2), word2)
+            word4 = array_xor_array(self.get_word(3), word3)
+
+            self.key_add_word(word1)
+            self.key_add_word(word2)
+            self.key_add_word(word3)
+            self.key_add_word(word4)
+
+        return self.key
---
+++ utils.py
@@ -0,0 +1,40 @@
+from variables import l_table, e_table
+
+def string_to_hex_array(text: str) -> list:
+    
+    return [hex(ord(text[i])) for i in range(len(text))]
+
+def array_split(array: list, n: int) -> list:
+
+    return [array[i:i + n] for i in range(0, len(array), n)]
+
+def array_to_matrix(text):
+
+    return [[text[i], text[i+4], text[i+8], text[i+12]] for i in range(4)]
+
+def matrix_to_array(matrix):
+
+    new_list = []
+    for i in matrix:
+        for j in i:
+            new_list.append(j)
+    return new_list
+
+def array_xor_array(a, b):
+
+    return [hex(int(i, 16) ^ int(j, 16)) for i, j in zip(a, b)]
+
+def galois_multiplication(a, b):
+
+    value = 0
+
+    if a == 0 or b == 0:
+        return 0
+    elif a == 1:
+        return b
+    elif b == 1:
+        return a
+    else:
+        value = l_table[a] + l_table[b]
+
+    return e_table[value % 255]
